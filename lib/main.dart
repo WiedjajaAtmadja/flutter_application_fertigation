0b6e9424ee69705c1bf8a9e14d75563d36c38d09
Catpra <fadhil.saeho@binus.ac.id>
2024-06-16 03:51:38 +0700

    show schedule on home page and adding cards on input page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Map<String, String>> schedules = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: MyHomePage(title: 'Home Page', schedules: schedules),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -16,19 +35,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, String>> schedules = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: widget.schedules.isEmpty
+      body: schedules.isEmpty
           ? Center(child: Text('There are no added schedules'))
           : ListView.builder(
-              itemCount: widget.schedules.length,
+              itemCount: schedules.length,
               itemBuilder: (context, index) {
-                final schedule = widget.schedules[index];
-                // return a widget to display the schedule
+                return Card(
+                  // Add this
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15), // Add this
+                  ),
+                  child: ListTile(
+                    title: Text(schedules[index]
+                        .entries
+                        .map((e) => '${e.key}: ${e.value}')
+                        .join(', ')),
+                  ),
+                );
               },
             ),
       floatingActionButton: FloatingActionButton(
@@ -39,27 +70,12 @@ class _MyHomePageState extends State<MyHomePage> {
           );
           if (newSchedule != null) {
             setState(() {
-              widget.schedules.add(newSchedule);
+              schedules.add(newSchedule);
             });
           }
         },
         child: Icon(Icons.add),
       ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Farm Scheduler',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(title: 'Farm Scheduler Home Page', schedules: []),
     );
   }
 }
@@ -96,77 +112,131 @@ class _NewSchedulePageState extends State<NewSchedulePage> {
           key: _formKey,
           child: Column(
             children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Hour',
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.number,
-                onSaved: (value) => hour = value!,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter an hour';
-                  }
-                  final int? hour = int.tryParse(value);
-                  if (hour == null || hour < 0 || hour > 23) {
-                    return 'Please enter a valid hour (0-23)';
-                  }
-                  return null;
-                },
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Hour',
+                    ),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) => hour = value!,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter an hour';
+                      }
+                      final int? hour = int.tryParse(value);
+                      if (hour == null || hour < 0 || hour > 23) {
+                        return 'Please enter a valid hour (0-23)';
+                      }
+                      return null;
+                    }),
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Minute',
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15), // Add this
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.number,
-                onSaved: (value) => minute = value!,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter a minute';
-                  }
-                  final int? minute = int.tryParse(value);
-                  if (minute == null || minute < 0 || minute > 59) {
-                    return 'Please enter a valid minute (0-59)';
-                  }
-                  return null;
-                },
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Minute',
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSaved: (value) => minute = value!,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a minute';
+                    }
+                    final int? minute = int.tryParse(value);
+                    if (minute == null || minute < 0 || minute > 59) {
+                      return 'Please enter a valid minute (0-59)';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Duration',
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15), // Add this
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.number,
-                onSaved: (value) => duration = value!,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter a duration';
-                  }
-                  final int? duration = int.tryParse(value);
-                  if (duration == null || duration < 0 || duration > 60) {
-                    return 'Please enter a valid duration (0-60)';
-                  }
-                  return null;
-                },
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Duration',
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSaved: (value) => duration = value!,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a duration';
+                    }
+                    final int? duration = int.tryParse(value);
+                    if (duration == null || duration < 0 || duration > 60) {
+                      return 'Please enter a valid duration (0-60)';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Area',
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                items: ['1', '2', '3'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  // Add your logic here
-                },
-                onSaved: (value) => area = value!,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please select an area';
-                  }
-                  return null;
-                },
+                child: DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      labelText: 'Area',
+                    ),
+                    items: ['1', '2', '3'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      // Add your logic here
+                    },
+                    onSaved: (value) => area = value!,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select an area';
+                      }
+                      return null;
+                    }),
               ),
               ElevatedButton(
                 onPressed: () {
